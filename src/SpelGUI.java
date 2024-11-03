1011c6d2500102fc7aff9ff1d1b75774a8fd2fa0
Lucas Frykman <lucasfrykmaan@gmail.com>
2024-11-03 18:26:42 +0100

    la till knapp för start

--- src/SpelGUI.java
+++ src/SpelGUI.java
@@ -1,16 +1,25 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SpelGUI extends JFrame implements ActionListener {
     public SpelGUI(){
+        setTitle("15-Spel");
         JPanel panel = new JPanel();
-        setVisible(true);
-        add(panel);
+        panel.setLayout(new GridLayout(4,4));
+
+        JPanel buttonPanel = new JPanel();
+        JButton startButton = new JButton("Nytt spel");
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        buttonPanel.add(startButton);
+
+        add(buttonPanel, BorderLayout.NORTH);
+        add(panel,BorderLayout.CENTER);
         setSize(500,500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        System.out.println("test");
+        setVisible(true);
 
     }
 
