51a8cc13eb90667f06f5603cba906d1fb531f8aa
Lucas Frykman <lucasfrykmaan@gmail.com>
2024-11-04 16:29:50 +0100

    la till shuffle på nummer, la till solvedmetod och facitmetod

--- src/SpelGUI.java
+++ src/SpelGUI.java
@@ -6,49 +6,107 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class SpelGUI extends JFrame implements ActionListener {
-    ArrayList<JButton> buttons = new ArrayList<>();
+    private ArrayList<JButton> buttons = new ArrayList<>();
+    private ArrayList<String> buttonAnswer = new ArrayList<>();
     private JPanel panel;
 
     public SpelGUI() {
+        for (int i = 1; i <= 15; i++) {
+            buttonAnswer.add(Integer.toString(i));
+        }
+        buttonAnswer.add(" ");
+
         setTitle("15-Spel");
         panel = new JPanel();
         panel.setLayout(new GridLayout(4, 4));
-        this.createButtons();
 
         JPanel buttonPanel = new JPanel();
         JButton startButton = new JButton("Nytt spel");
         buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
         buttonPanel.add(startButton);
+        startButton.addActionListener(this);
+
+        JButton facit = new JButton("Facit");
+        buttonPanel.add(facit);
+        facit.addActionListener(this);
+
+
 
         add(buttonPanel, BorderLayout.NORTH);
         add(panel, BorderLayout.CENTER);
         setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
         setVisible(true);
         this.pack();
-
+        setLocationRelativeTo(null);
     }
 
     public static void main(String[] args) {
         SpelGUI spel = new SpelGUI();
     }
+    public boolean isSolved(){
+        for (int i = 0; i < buttons.size(); i++) {
+            if (!buttons.get(i).getText().equals(buttonAnswer.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public void createButtons() {
-        for (int n = 1; n <= 15; n++) {
-            JButton button = new JButton(Integer.toString(n));
+        buttons.clear();
+        panel.removeAll();
+        ArrayList<String> numbers = new ArrayList<>(buttonAnswer);
+        Collections.shuffle(numbers);
+        for (String number : numbers) {
+            JButton button = new JButton(number);
             button.addActionListener(this);
+            if (number.equals(" ")) {
+                button.setEnabled(false);
+            }
             buttons.add(button);
             panel.add(button);
+
         }
-        JButton emptyButton = new JButton(" ");
-        panel.add(emptyButton);
-        buttons.add(emptyButton);
-        System.out.println("xael");
+        panel.repaint();
+        panel.revalidate();
+        pack();
+        setLocationRelativeTo(null);
     }
+    public void showFacit(){
+        buttons.clear();
+        panel.removeAll();
+        ArrayList<String> numbers = new ArrayList<>(buttonAnswer);
+
+        for (String number : numbers) {
+            JButton button = new JButton(number);
+            button.addActionListener(this);
+            if (number.equals(" ")) {
+                button.setEnabled(false);
+            }
+            buttons.add(button);
+            panel.add(button);
+
+        }
+        panel.repaint();
+        panel.revalidate();
+        pack();
+        setLocationRelativeTo(null);
+    }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        JButton buttonStart = (JButton) e.getSource();
+        if (buttonStart.getText().equals("Nytt spel")) {
+            this.createButtons();
+            panel.repaint();
+            panel.revalidate();
+            pack();
+        } else if (buttonStart.getText().equals("Facit")) {
+            this.showFacit();
+        }
+
         for (int i = 0; i < buttons.size(); i++) {
             if (e.getActionCommand().equals(buttons.get(i).getActionCommand())) {
                 if (i - 4 >= 0 && buttons.get(i - 4).getActionCommand().equals(" ")) {
@@ -70,10 +128,15 @@ public class SpelGUI extends JFrame implements ActionListener {
                 panel.revalidate();
                 panel.repaint();
 
+                if (isSolved()){
+                    JOptionPane.showMessageDialog(this, "Du har vunnit");
+                }
+
                 break;
 
+
+            }
             }
         }
 
     }
-}
